b1989b677609013053706bda103bb3706e90e611
Omer Sefa <omercarikci204@gmail.com>
2025-08-15 20:46:53 +0300

    C# Fundamentals

---
+++ Algorithms and Programming with C#/Library Project/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int toplamfiyat=0;
+            string secim;
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("-- Türkçe Kitaplar        --  Yabancı Kitaplar --");
+            Console.WriteLine();
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("-- 1-Çalıkuşu: Reşat Nuri  --  2-Tuna Kılavuzu:  Jules Verne --");
+            Console.WriteLine();
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine("İşlemler Menüsü");
+            Console.WriteLine();
+            Console.WriteLine("1-Fiyat Sorgulama");
+            Console.WriteLine("2-Yeni Okur Kaydı");
+            Console.WriteLine("3-Günün Kitabı");
+            Console.WriteLine("4-Kitap Arşivi");
+            Console.WriteLine("5-Kitap Satın Al");
+            Console.WriteLine("6-Oyun");
+            Console.WriteLine();
+            Console.Write("Yapmak istediğiniz işlem numarasını giriniz: ");
+
+            char islem;
+            islem = Char.Parse(Console.ReadLine());
+            if (islem == '1')
+            {
+                Console.WriteLine();
+                Console.Write("Lütfen kitap numarası giriniz: ");
+                string numara;
+                numara = Console.ReadLine();
+                switch (numara)
+                {
+                    case "1":Console.Write("Çalıkuşu -> 12TL");break;
+                    case "2": Console.Write("Tuna Kılavuzu -> 20TL");break;
+                    default: Console.Write("Bu numaraya ait kitap bulunmamaktadır.");break;
+                }
+
+            }
+            if (islem == '2')
+            {
+                Console.WriteLine("------------ Kayıt Ol -----------");
+                string ad, soyad, universite;
+                Console.Write("Adınızı giriniz: ");
+                ad = Console.ReadLine();
+                Console.Write("Soyadınızı giriniz: ");
+                soyad = Console.ReadLine();
+                Console.Write("Üniversite bilgisini giriniz: ");
+                universite = Console.ReadLine();
+
+                string dosya = ("C:\\adres\\kullanici.txt");
+                StreamWriter streamWriter = new StreamWriter(dosya);
+                streamWriter.WriteLine(ad + " " + soyad);
+                streamWriter.WriteLine(universite);
+                streamWriter.Close();
+
+            }
+            if (islem == '3')
+            {
+                Console.WriteLine();
+                Console.WriteLine("Bugünün kitabı: Tuna Kılavuzu");
+            }
+            if (islem == '4')
+            {
+                Console.WriteLine();
+                FileStream fs = new FileStream("adres",FileMode.Open,FileAccess.Read);
+                StreamReader sr = new StreamReader(fs);
+                string metin = sr.ReadLine();
+                while (metin != null)
+                {
+                    Console.WriteLine(metin);
+                    metin = sr.ReadLine();
+                }
+                sr.Close();
+                fs.Close();
+            }
+            if (islem == '5')
+            {
+                for(int i = 1; i <= 100; i++)
+                {
+                    Console.WriteLine();
+                    Console.Write("Alacağınız kitabın numarası: ");
+                    secim = Console.ReadLine();
+                    if(secim == "1")
+                    {
+                        toplamfiyat = toplamfiyat + 12;
+                    }
+                    else if (secim == "2")
+                    {
+                        toplamfiyat = toplamfiyat + 14;
+                    }
+                    Console.WriteLine("Toplam tutar: " + toplamfiyat);
+
+                }
+            }
+            if (islem == '6')
+            {
+                Console.WriteLine();
+                int tahmin=0;
+                Random rnd = new Random();
+                int sayi = rnd.Next(1,100);
+                while (sayi != tahmin)
+                {
+                    Console.Write("Sayı giriniz: ");
+                    tahmin = Convert.ToInt32(Console.ReadLine());
+                    if(tahmin > sayi)
+                    {
+                        Console.Write("Daha küçük");
+                    }
+                    if (tahmin < sayi)
+                    {
+                        Console.Write("Daha büyük");
+                    }
+                    if (tahmin == sayi)
+                    {
+                        Console.Write("Sayıyı doğru tahmin ettiniz.");
+                    }
+
+                }
+            }
+            Console.Read();
+
+        }
+    }
+}
---
+++ Algorithms and Programming with C#/Library Project/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Library Project")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Library Project")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4a355439-cab6-42be-a426-f1eac5511b1c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
